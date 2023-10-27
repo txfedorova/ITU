d38694d272bfce4aa0036bc287d6eea545c41fbb
rediso4ka <swimmer030203@gmail.com>
2023-10-27 16:31:56 +0200

    added users input

---
+++ lib/model/user_input.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+class UserInputWidget extends StatefulWidget {
+  @override
+  _UserInputWidgetState createState() => _UserInputWidgetState();
+}
+
+class _UserInputWidgetState extends State<UserInputWidget> {
+  final TextEditingController _nameController = TextEditingController();
+  final List<String> users = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Column(
+          children: users.asMap().entries.map((entry) {
+            final int index = entry.key;
+            final String user = entry.value;
+            return Container(
+              height: 55,
+              child: Card(
+                margin: const EdgeInsets.all(3.0), // Add margin for spacing
+                child: ListTile(
+                  title: Text(user),
+                  trailing: IconButton(
+                    icon: Icon(Icons.close), // X icon for delete
+                    onPressed: () {
+                      // Remove the user from the list
+                      setState(() {
+                        users.removeAt(index);
+                      });
+                    },
+                  ),
+                ),
+              ),
+            );
+          }).toList(),
+        ),
+        Container(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: TextField(
+            controller: _nameController,
+            decoration: const InputDecoration(
+              hintText: 'User Name', // Display the hint text
+              hintStyle: TextStyle(color: Colors.grey), // Style for the hint text
+              focusedBorder: UnderlineInputBorder(
+                borderSide:
+                    BorderSide(color: Colors.black), // Border color when focused
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(height: 6),
+        ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            shape: RoundedRectangleBorder(
+              borderRadius:
+                  BorderRadius.circular(20), // Adjust the radius as needed
+            ),
+            padding: const EdgeInsets.all(14),
+            backgroundColor: const Color(0xFF59B773),
+            foregroundColor: Colors.white,
+          ),
+          onPressed: () {
+            final userName = _nameController.text;
+            if (userName.isNotEmpty) {
+              setState(() {
+                users.add(userName);
+              });
+              // Add user to your data source or perform the necessary action
+              // You can use context.read<UserController>().addUser(userName);
+              _nameController.clear(); // Clear the input field
+            }
+          },
+          child: const Text(
+            'Add a new user',
+            style: TextStyle(fontSize: 18),
+          ),
+        )
+      ],
+    );
+  }
+}
--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:itu_app/model/user_input.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -7,22 +8,36 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
-        title: const Text('Home Screen'),
+        backgroundColor: const Color(0xFF59B773),
+        title: const Text('CineMatch'),
       ),
       body: Center(
         child: Container(
-          decoration: const BoxDecoration(
-            color: Colors.amber,
-          ),
-          //width: double.infinity,
+          color: const Color(0xFFF6FAF1),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
+              // User input section with scrolling
+              Expanded(
+                child: ListView(
+                  //shrinkWrap: true,
+                  children: [
+                    // Include the UserInputWidget here
+                    UserInputWidget(), // This is the user input section
+                  ],
+                ),
+              ),
+
+              // Spacer to separate the user input section from the buttons
+              //Spacer(),
+
+              // Buttons at the bottom
               ElevatedButton(
                 style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.green,
+                  backgroundColor: const Color(0xFF59B773),
                   foregroundColor: Colors.white,
                 ),
                 child: const Text('Next User'),
@@ -30,10 +45,9 @@ class HomeScreen extends StatelessWidget {
                   context.push('/filmGallery');
                 },
               ),
-              
               ElevatedButton(
                 style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.green,
+                  backgroundColor: const Color(0xFF59B773),
                   foregroundColor: Colors.white,
                 ),
                 child: const Text('List films'),
